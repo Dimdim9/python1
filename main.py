90c2204564015c87af2fd9bb424a9d355981f366
Dimdim9 <madi.hamidou13@gmail.com>
2024-10-04 11:35:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+'''def miroir1(ch):
+    inv = ''
+    for e in ch:
+       inv = inv + e
+    return inv    
+print (miroir1('madi'))'''
+
+'''def miroire2 (ch):
+  inv = ''
+  for e in ch:
+    inv = e + inv 
+  return inv 
+print (miroire2('madi'))'''
